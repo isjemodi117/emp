7f8cbcba31d09adf154a3c3de8302aea617b8168
Kemblealucha <255038@unasat.sr>
2026-05-24 20:17:43 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,12 @@
+import mysql from 'mysql2/promise';
 
+export const db = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_password || '',
+  database: process.env.DB_NAME || 'medical_portal',
+  port: Numer(process.env.DB_PORT || 3306,
+  multipleStatements: true,
+  connectionLimit: 10,
+              });
+                                   
